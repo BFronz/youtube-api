e1d76e2f0fd01d382dc31335af599a9ad1603f9e
BFronz <bfronz1960@me.com>
2020-06-05 19:24:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import  SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
+
+
+class App extends React.Component {
+
+    render(){
+    
+        return(
+            <Grid justify="center" containter spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+
+                        <Grid item xs={12}>
+                            <SearchBar />
+                            <Grid item xs={8}>
+                              <VideoDetail />
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                {/* video list */}
+                            </Grid>
+
+                        </Grid>
+                    </Grid>    
+                </Grid>
+            </Grid>
+        )
+
+    }
+
+
 }
 
+
+
+// const App = () => {
+//     return(<h1>YouTuebe Clone App</h1>)
+// }
+
+
 export default App;
+
+
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const VideoDetail = () => {
+
+    return(
+
+        <h1>this is a video detail component</h1>
+
+    );
+
+
+}
+
+
+export default VideoDetail; 
